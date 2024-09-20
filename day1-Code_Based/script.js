f610b2af2e0dcbed650d9e1a133ad2069434d419
Sriram <136231040+SriramB16@users.noreply.github.com>
2024-09-20 23:04:06 +0530

    code based problems easy level finished

---
+++ day1-Code_Based/script.js
@@ -0,0 +1,16 @@
+// 1.Write a program that prints "Hello, World!" to the console.
+
+console.log("Hello, World!")
+
+// 2.Create a variable, assign a number to it, and log its value.
+
+var num = 8;
+console.log(num);
+
+// 3.Create a function that adds two numbers and returns the result.
+
+function addNum(n1,n2){
+    return n1 + n2;
+}
+
+console.log(addNum(2,3));
