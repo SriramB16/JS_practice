b3edc3ee989a428d507ced9ac21c6fd8c3e74bf4
Sriram <136231040+SriramB16@users.noreply.github.com>
2024-09-20 23:27:04 +0530

    code based problems medium level finished

--- day1-Code_Based/script.js
+++ day1-Code_Based/script.js
@@ -13,4 +13,33 @@ function addNum(n1,n2){
     return n1 + n2;
 }
 
-console.log(addNum(2,3));
+// console.log(addNum(2,3));
+
+// 4. Write a function to check if a number is even or odd.
+
+function check(n){
+    return n%2 === 0 ? 'even' : 'odd';
+}
+
+// 5. Create a function that multiplies two numbers without using the * operator.
+
+function multiply(n1,n2){
+    return n1/(1/n2);
+}
+
+// console.log(multiply(2,3))
+
+// 6. Write a program that calculates the area of a circle. 
+
+function circleArea(r){
+    return 3.14 * Math.pow(r,2);
+}
+
+
+// 7. Convert a temperature from Fahrenheit to Celsius
+
+function temperature(f){
+    return (f - 32) * 5/9
+}
+
+console.log(temperature(50))
